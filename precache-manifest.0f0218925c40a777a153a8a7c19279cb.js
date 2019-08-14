7947002d987a50a5ec3fdc2baff27dbc5d067260
MikeDRoss <michael4725@gmail.com>
2019-08-13 22:29:22 -0400

    Updates

---
+++ precache-manifest.0f0218925c40a777a153a8a7c19279cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf81f2086d94ba7e32cc683a5dfccd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997c9c81d6f5d0f3ad06",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b64d0906de652bbe8ae6",
+    "url": "/static/css/main.f0ab3b04.chunk.css"
+  },
+  {
+    "revision": "997c9c81d6f5d0f3ad06",
+    "url": "/static/js/2.ce9db070.chunk.js"
+  },
+  {
+    "revision": "b64d0906de652bbe8ae6",
+    "url": "/static/js/main.049cbff2.chunk.js"
+  },
+  {
+    "revision": "737425d3abf597188c5e",
+    "url": "/static/js/runtime~main.46ff90e1.js"
+  },
+  {
+    "revision": "f3388a312a99e1ae1ccf5286be253eb1",
+    "url": "/static/media/Profile_Pic.f3388a31.png"
+  },
+  {
+    "revision": "75afe06ec8efd08fef78c3acccd2eefc",
+    "url": "/static/media/aws.75afe06e.png"
+  },
+  {
+    "revision": "17879ce991f09eddaf0fb3ed1d07d332",
+    "url": "/static/media/penn-state.17879ce9.png"
+  }
+]);
