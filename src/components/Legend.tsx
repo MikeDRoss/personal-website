09899910677f1aa37da2bce3499f8264b4dee5cf
MikeDRoss <michael4725@gmail.com>
2019-08-19 08:48:08 -0400

    Add tooltip to map for country names

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import legendKeys from '../data/LegendData';
 
 const Legend:React.FC = () => (
-<div className='map-overlay' id='legend'>
-    {legendKeys.map(legendKey => ([
-        <div>
-            <span className='legend-key' style={{backgroundColor: legendKey.color}}></span>
-            <span>{legendKey.key}</span>
-        </div>
-    ])
-    )}
-    
-</div>
+    <div className='map-overlay' id='legend'>
+        {legendKeys.map(legendKey => ([
+            <div key={legendKey.key}>
+                <span className='legend-key' style={{backgroundColor: legendKey.color}}></span>
+                <span>{legendKey.key}</span>
+            </div>
+        ])
+        )}
+    </div>
 );
 
 export default Legend;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -11,6 +11,9 @@ const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN || '';
 const initialState = {
     mapStyle: defaultMapStyle,
     mapData: null,
+    hoveredFeature: null,
+    x: 0,
+    y: 0,
     viewport: {
         height:600,
         width:'100%',
@@ -26,6 +29,29 @@ type Viewport = typeof initialState.viewport;
 
 export default class Map extends React.Component<{}, State> {
     public state: State = initialState;
+
+    public onHover = (event:any) => {
+        const {
+          features,
+          srcEvent: {offsetX, offsetY}
+        } = event;
+        const hoveredFeature = features && features.find((f:any) => f.source === 'countryLayer');
+    
+        this.setState({hoveredFeature, x: offsetX, y: offsetY});
+      };
+
+      public renderTooltip = () => {
+        const {hoveredFeature, x, y} = this.state;
+
+        return (
+          hoveredFeature && (
+            <div className="tooltip" style={{left: x, top: y}}>
+              <div>{(hoveredFeature as any).properties.name}</div>
+            </div>
+          )
+        );
+      }
+
 
     public setMapLayers = () => {
         json(process.env.PUBLIC_URL + '/map-data.geojson', (error, mapData : any) => {
@@ -44,7 +70,9 @@ export default class Map extends React.Component<{}, State> {
                 mapboxApiAccessToken={MAPBOX_TOKEN}
                 onLoad={() => this.setMapLayers()}
                 onViewportChange={(viewport:any )=> this.setState({ viewport })}
+                onHover={this.onHover}
             >
+            {this.renderTooltip()}
             <Legend />
             </ReactMapGL>
         );
