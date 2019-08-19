16787b0de9cfbc10ae795351e4bfc2ae647e6ea8
MikeDRoss <michael4725@gmail.com>
2019-08-18 23:51:08 -0400

    add Polygon shaped visited countries to geojson

--- src/data/MapData.tsx
+++ src/data/MapData.tsx
@@ -8,7 +8,7 @@ const fillLayer = fromJS({
     interactive: true,
     paint: {
       'fill-color': '#008000',
-      'fill-outline-color': '#008000',
+      'fill-outline-color': '#000000',
       'fill-opacity': 0.3
     }
   });
@@ -17,8 +17,8 @@ export const defaultMapStyle = fromJS(MAP_STYLE);
 
 export const generateMapStyle = (data: any) => defaultMapStyle
     .setIn(['sources', 'countryLayer'], fromJS({
-    type: 'geojson',
-    data
+        type: 'geojson',
+        data
     }))
     .set('layers', defaultMapStyle.get('layers').push(fillLayer));
 
